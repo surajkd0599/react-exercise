7c1bf0baee3b87fdd3e50134e2359d9189656313
suraj <surajkd050599@gmail.com>
2020-04-30 09:26:54 +0530

    exercise completed

--- es6Practice/app/index.js
+++ es6Practice/app/index.js
@@ -42,13 +42,13 @@ d. Map to an array of seconds
 e. Reduce to get total using .filter and .map
 */
 
-/*
-console.log(addIntoArray())
-console.log(findFlexItems())
-console.log(mapOfTimeStrings())
-console.log(arrayOfSec())
-console.log(getTotal())
-*/
+
+addIntoArray()
+findFlexItems()
+mapOfTimeStrings()
+arrayOfSec()
+getTotal()
+
 
 
 /*Q3. Create a markup template using string literal
@@ -106,7 +106,7 @@ phoneNo: 9999999999
 
 let {Address} = user;
 let key = Object.keys(Address);
-console.log(key);
+console.log("Destructuring : ",key);
 */
 
 
@@ -116,7 +116,7 @@ console.log(key);
 //Q4. Filter unique array members using Set.
 /*let array1 = [1,2,2,3,4,1,5,3,5,6,7];
 let set = new Set(array1);
-console.log(set);
+console.log("Unique array using set : ",set);
 */
 //Q5. Find the possible combinations of a string and store them in a MAP? 
 
@@ -191,73 +191,128 @@ console.log(a)
 //Q8. Import a module containing the constants and method for calculating area of circle, rectangle, cylinder.
 
 
-console.log(circle(5));
+console.log("Area of circle : ",circle(5));
 
-console.log(rectangle(2,5));
+console.log("Area of rectangle : ",rectangle(2,5));
 
-console.log(tsaCylinder(5,6))
+console.log("Area of cylinder : ",tsaCylinder(5,6))
 
 //Q9. Import a module for filtering unique elements in an array.
-//import filtering from './filtering.js'
+import filtering from './filtering.js'
 
-//let array2 = [1,1,2,3,4,5,6,1,3,2,3,4,7]
+let array2 = [1,1,2,3,4,5,6,1,3,2,3,4,7]
 
-//console.log(filtering(array2));
+console.log(filtering("Filtered Array is : ",array2));
 
 //Q10. Write a program to flatten a nested array to single level using arrow functions.
 /*var arrays = [["$6"], ["$12"], ["$25"], ["$25"], ["$18"], ["$22"], ["$10"], ["$0"], ["$15"],["$3"], ["$75"], ["$5"], ["$100"], ["$7"], ["$3"], ["$75"], ["$5"]];
 
 arrays = arrays.reduce((a, b) => a.concat(b), []);
-console.log(arrays)
+console.log("Flattened array is : ",arrays)
 */
 
 //Q11. Implement a singly linked list in es6 and implement addFirst() addLast(), length(), getFirst(), getLast(). (without using array)
 
-/*class Node{
-  constructor(data, next = null){
-      this.data = data,
-      this.next = next
+class Node{
+  constructor(data, next=null)
+  {
+      this.data=data,
+      this.next=next;
   }
 }
 
 class LinkedList{
-  constructor(){
-      this.head = null;
+  constructor()
+  {
+      this.head=null;
   }
 }
 
-let list = new LinkedList();
+let count=0;
 
-//Add first
-LinkedList.prototype.addFirst = function(data){
-      let newNode = new Node(data);
-  
-      newNode.next = this.head;
-      
-      this.head = newNode;
+//add First
+LinkedList.prototype.addFirst = function(data)
+{
+  let newNode= new Node(data);
+  newNode.next=this.head;
+  this.head=newNode;
+  count++;
+  return this.head;
+}
+
+//add Last
+LinkedList.prototype.addLast=function(data)
+{
+  let newNode=new Node(data);
+
+  if(!this.head)
+  {
+      this.head=newNode;
+      count++;
       return this.head;
   }
 
-//Add last
-LinkedList.prototype.addLast = function(data){
-      
-      let newNode = new Node(data);
-  
-      if(!this.head){
-          this.head = newNode;
-          return this.head;
+  else{
+      let last= this.head;
+      while(last.next!==null)
+      {
+          last=last.next;
       }
-  
-     let tail = this.head;
-     while(tail.next !== null){
-          tail = tail.next;
-     }
-     tail.next = newNode;
-     return this.head;
-  }
-  */
 
-//for lenght
+      last.next=newNode;
+      count++;
+      return this.head;
+  }
+}
+
+//get length
+LinkedList.prototype.length= function()
+{
+ return count;
+}
+
+//get first 
+LinkedList.prototype.getFirst= function()
+{
+  return this.head.data;
+}
+
+//get last
+LinkedList.prototype.getLast= function()
+{
+  let last= this.head;
+      while(last.next!==null)
+      {
+          last=last.next;
+      }
+
+      return last.data;
+}
+
+//print statement
+LinkedList.prototype.print = function()
+{
+  let last = this.head;
+  while(last.next!=null)
+  {
+      console.log(last.data);
+      last=last.next;
+
+  }
+
+  console.log(last.data);
+}
+
+let ll= new LinkedList();
+ll.addFirst(67);
+ll.addFirst(90);
+ll.addLast(89);
+console.log(ll);
+console.log("Length of Linked List",ll.length());
+console.log("First element:",ll.getFirst());
+console.log("Last Element:",ll.getLast());
+console.log("Elements in Linked List:");
+ll.print();
 
 //Q12. Implement Map and Set using Es6
 
@@ -286,3 +341,83 @@ console.log(map);
 */
 
 //Q13. Implementation of stack (using linked list)
+function stackLL(){
+    
+  let Node = function(element){
+    this.element = element;
+    this.next = null;
+  }
+
+  let size = 0;
+  let head = null;
+  
+  this.push = function(element){
+    
+    let node = new Node(element),current;
+    
+    current = head;
+    node.next = current;
+    head = node;
+    
+    size++;
+  }
+  
+
+  this.pop = function(){
+    let current = head;
+    
+   if(current){
+      let ele = current.element;
+      current = current.next;
+      head = current;
+      size--;
+      return ele;
+    }
+    
+    return null;   
+  }
+  
+
+  this.peek = function(){    
+    if(head){    
+      return head.element;
+    }
+
+    return null;
+  }
+  
+  this.print = function()
+{
+  let last = head;
+  while(last.next!=null)
+  {
+      console.log(last.element);
+      last=last.next;
+
+  }
+
+  console.log(last.element);
+}
+
+  this.isEmpty = function(){
+    return size === 0;
+    }
+    
+    
+  this.size = function(){
+    return size;
+  }
+}
+
+let stack = new stackLL();
+stack.push(56);
+stack.push(78);
+stack.pop();
+stack.push(80);
+stack.push(93);
+
+console.log("Elememts in stack using linked List : ");
+stack.print()
+console.log("Is stack empty? (true/false) ? ",stack.isEmpty());
+console.log("size of stack is  " + stack.size());
+console.log("Peek Element:",stack.peek());
