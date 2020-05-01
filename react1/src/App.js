a128a82dc127aadc9780b849051c53caa3ad72b8
suraj <surajkd050599@gmail.com>
2020-05-01 13:45:49 +0530

    code formatted

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,94 +1,79 @@
-import React, {
-    Component
-} from 'react';
-import Fruit from './Fruit/Fruit';
+import React, { Component } from "react";
+import Fruit from "./Fruit/Fruit";
 
 class App extends Component {
-    state = {
-        inputValue: '',
-        fruits: []
-    }
+  state = {
+    inputValue: "",
+    fruits: [],
+  };
 
-    onInputChange(event) {
-        this.setState({
-            inputValue: event.target.value,
-        });
-    }
+  onInputChange(event) {
+    this.setState({
+      inputValue: event.target.value,
+    });
+  }
 
-    deleteFruitHandler = (fruitIndex) => {
-        const index = this.state.fruits.findIndex((currentIndex) => currentIndex.key === fruitIndex);
-        let fruits = [...this.state.fruits];
-        fruits.splice(index, 1);
-        this.setState({
-            fruits: fruits
-        });
-    }
-
-    addItem() {
-        let fruits = this.state.fruits;
-        let data = this.state.inputValue.split('-')
-        const fruitQuantity = {
-            key: Math.floor(Math.random() * 30),
-            name: data[0],
-            quantity: data[1]
-        }
-
-        fruits.push(fruitQuantity);
-        this.setState({
-            fruits: fruits
-        });
-    }
-
-
-    render() {
-
-        const style = {
-            border: '1px solid black',
-            borderCollapse: 'collapse'
-        }
-
-        let fruits = (
-            this.state.fruits.map((fruit) => {
-                return ( <
-                    table style = {
-                        style
-                    }
-                    key = {
-                        fruit.key
-                    } >
-                    <
-                    Fruit name = {
-                        fruit.name
-                    }
-                    quantity = {
-                        fruit.quantity
-                    }
-                    click = {
-                        () => this.deleteFruitHandler(fruit.key)
-                    }
-                    />  
-                </table>
-                )
-            })
-        )
-
-        return ( < div >
-                <
-                input type = "text"
-                onChange = {
-                    (event) => this.onInputChange(event)
-                }
-                /><br/ >
-                <
-                button type = "submit"
-                onClick = {
-                    () => this.addItem()
-                } > SUBMIT </button> {
-                fruits
-            } 
-        </div>
+  deleteFruitHandler = (fruitIndex) => {
+    const index = this.state.fruits.findIndex(
+      (currentIndex) => currentIndex.key === fruitIndex
     );
-}
+    let fruits = [...this.state.fruits];
+    fruits.splice(index, 1);
+    this.setState({
+      fruits: fruits,
+    });
+  };
+
+  addItem(e) {
+    e.preventDefault();
+    let fruits = this.state.fruits;
+    let data = this.state.inputValue.split("-");
+    const fruitQuantity = {
+      key: Math.floor(Math.random() * 30),
+      name: data[0],
+      quantity: data[1],
+    };
+
+    fruits.push(fruitQuantity);
+    this.setState({
+      inputValue: "",
+      fruits: fruits,
+    });
+  }
+
+  render() {
+    const style = {
+      border: "1px solid black",
+      borderCollapse: "collapse",
+    };
+
+    let fruits = this.state.fruits.map((fruit) => {
+      return (
+        <table style={style} key={fruit.key}>
+          <Fruit
+            name={fruit.name}
+            quantity={fruit.quantity}
+            click={() => this.deleteFruitHandler(fruit.key)}
+          />
+        </table>
+      );
+    });
+
+    return (
+      <form>
+        <input
+          type="text"
+          onChange={(event) => this.onInputChange(event)}
+          value={this.state.inputValue}
+        />
+        <br />
+        <button type="submit" onClick={(e) => this.addItem(e)}>
+          SUBMIT
+        </button>
+        {fruits}
+      </form>
+    );
+  }
 }
 
 export default App;
--- react1/src/Fruit/Fruit.js
+++ react1/src/Fruit/Fruit.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import './Fruit.css'
+import React from "react";
+import "./Fruit.css";
 
 const fruit = (props) => {
-    return ( <
-        tbody >
-        <
-        tr >
-        <
-        td className = 'all' > {
-            props.name
-        } </td> <
-        td className = 'all' > {
-            props.quantity
-        } </td> <
-        td className = 'all'
-        onClick = {
-            props.click
-        } > delete </td> </tr> 
-        </tbody>
-    )
-
-}
+  return (
+    <tbody>
+      <tr>
+        <td className="all"> {props.name} </td>
+        <td className="all"> {props.quantity} </td>
+        <td className="all" onClick={props.click}>
+          delete
+        </td>
+      </tr>
+    </tbody>
+  );
+};
 
 export default fruit;
