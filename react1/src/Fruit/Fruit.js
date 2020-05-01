6af624c657b7aaece9105f772aaaed7a4e7c929a
suraj <surajkd050599@gmail.com>
2020-05-01 12:37:25 +0530

    react exercise 1

---
+++ react1/src/Fruit/Fruit.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Fruit.css'
+
+const fruit = (props) => {
+return (
+<tbody>
+<tr>
+<td className = 'all' >{props.name}</td>
+<td className = 'all' >{props.quantity}</td>
+<td className = 'all' onClick = {props.click}>delete</td>
+</tr>
+</tbody>
+)
+
+}
+
+export default fruit;
