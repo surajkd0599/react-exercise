725d1093bcdb822588c1a9485b0ce24bb9c1b9b3
suraj <surajkd050599@gmail.com>
2020-05-08 13:08:43 +0530

    ajax exercise done working operations with jsonplaceholder.typicode

---
+++ ajax/ajax1/src/axios.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const instance = axios.create(
+  (axios.defaults.baseURL = "https://jsonplaceholder.typicode.com")
+);
+
+instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN from instance'
+instance.defaults.headers.post['Content-Type'] = 'application/json'
+
+
+instance.interceptors.request.use(
+    (request) => {
+      console.log(request);
+      return request;
+    },
+    (error) => {
+      console.log(error);
+      return Promise.reject(error);
+    }
+  );
+  
+  instance.interceptors.response.use(
+    (response) => {
+      console.log(response);
+      return response;
+    },
+    (error) => {
+      console.log(error);
+      return Promise.reject(error);
+    }
+  );
+  
+export default instance;
